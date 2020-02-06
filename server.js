8920f985ca1c7fd19998c7088bddafa463dba0c0
sashidhar-meesala <sashidharklu@gmail.com>
2020-02-06 15:48:18 -0800

    added forms,styles

--- server.js
+++ server.js
@@ -13,6 +13,12 @@ app.get('/', function (req, res) {
     })
   })
 
+  app.get('/login', function (req, res) {
+    res.render("login",{
+        title:"login",
+        headingInfo:"login page lets get started",
+    })
+  })
   app.get('/register', function (req, res) {
     res.render("register",{
       title:"User Registration",
