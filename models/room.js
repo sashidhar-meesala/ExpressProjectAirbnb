d56934d4b533f6d6784ad7d55f15d9540bc6fb40
sashidhar-meesala <sashidharklu@gmail.com>
2020-03-02 22:37:56 -0500

    images added, model created , database added,

--- models/room.js
+++ models/room.js
@@ -1,69 +1,89 @@
-const featuredRooms =
+const room =
 {
 
-    places: [],
-
+    featuredDB : [],
+    listingDB : [],
     init()
     {
 
-    this.places.push(
-      {
-        imgURL:`/img/room1.jpg`,
-        title:`ENTIRE VILLA. 2 BEDS | Modern Branyan Guest Villa`,
-        price:100,
-        rating:5
-      });
-      this.places.push(
-        {
-          imgURL:`/img/room2.jpg`,
-          title:`ENTIRE HOUSE · 8 BEDS | Spacious Coastal Retreat`,
-          price:120,
-          rating:4.1
-  
-        });
-        this.places.push(
-          {
-            imgURL:`/img/room3.jpg`,
-            title:`ENTIRE BUNGALOW · 3 BEDS | Point Boston Beach House`,
-            price:200,
-            rating:5
+        this.featuredDB.push({image:`/img/room7.jpg`,title:'COZY ROOM | BAZAR, BALI',description:`Cozy room with lakeview
+        `,price:`$40 CAD/night`});
     
-          });
-        
-          this.places.push(
+        this.featuredDB.push({image:`/img/room8.jpg`,title:'VERIFIED | CASINO BLUE, BANGKOK',description:`secret property inside Casino`,price:`$110 CAD/night`});
+    
+        this. featuredDB.push({image:`/img/room9.jpg`,title:'ENTIRE VILLA | MELBOURNE',description:`Villa to hide out`,price:`$120 CAD/night`});
+
+
+        this. featuredDB.push({image:`/img/room10.jpg`,title:'BEST STAY | FIJIWI, HAVANA',description:`Spice up your weekend stay`,price:`$180 CAD/night`});
+
+        this.featuredDB.push({image:`/img/room11.jpg`,title:'PREMIUM HOUSE | MAIN STREET, VERDE',description:`A house to hide out from urban sky
+        `,price:`$110 CAD/night`});
+    
+        this.featuredDB.push({image:`/img/room12.jpg`,title:'EXCLUSIVE | ENTIRE HOUSE, HYDERABAD',description:`Lots and loads can happen here`,price:`$189 CAD/night`});
+
+        this.listingDB.push(
             {
-              imgURL:`/img/room4.jpg`,
-              title:`ENTIRE BUNGALOW · 4 BEDS | Beach Front Holiday Home `,
-              price:140,
-              rating:4.5
-      
+              imgURL:`/img/room1.jpg`,
+              title:`ENTIRE VILLA. 2 BEDS | Modern Branyan Guest Villa`,
+              price:100,
+              rating:5
             });
-        
-            this.places.push(
+            this.listingDB.push(
               {
-                imgURL:`/img/room5.jpg`,
-                title:`ENTIRE APARTMENT · 2 BEDS | The Loft - Crackenback`,
-                price:1270,
-                rating:4.4
+                imgURL:`/img/room2.jpg`,
+                title:`ENTIRE HOUSE · 8 BEDS | Spacious Coastal Retreat`,
+                price:120,
+                rating:4.1
         
               });
-        
-              this.places.push(
+              this.listingDB.push(
                 {
-                  imgURL:`/img/room6.jpg`,
-                  title:`ENTIRE APARTMENT · 4 BEDS |The Beach Apartment `,
-                  price:190,
-                  rating:4.3
+                  imgURL:`/img/room3.jpg`,
+                  title:`ENTIRE BUNGALOW · 3 BEDS | Point Boston Beach House`,
+                  price:200,
+                  rating:5
           
                 });
-            },
-            getallRooms()
-            {
-                return this.places;
-            }
-        
-        }
-        
-        
-        featuredRooms.init();
-        module.exports=featuredRooms;
+              
+                this.listingDB.push(
+                  {
+                    imgURL:`/img/room4.jpg`,
+                    title:`ENTIRE BUNGALOW · 4 BEDS | Beach Front Holiday Home `,
+                    price:140,
+                    rating:4.5
+            
+                  });
+              
+                  this.listingDB.push(
+                    {
+                      imgURL:`/img/room5.jpg`,
+                      title:`ENTIRE APARTMENT · 2 BEDS | The Loft - Crackenback`,
+                      price:1270,
+                      rating:4.4
+              
+                    });
+              
+                    this.listingDB.push(
+                      {
+                        imgURL:`/img/room6.jpg`,
+                        title:`ENTIRE APARTMENT · 4 BEDS |The Beach Apartment `,
+                        price:190,
+                        rating:4.3
+                
+                      });
+
+    },
+    getallFeaturedRooms()
+    {
+        return this.featuredDB;
+    },
+    
+    getallListingRoom(){
+        return this.listingDB;
+    },
+
+}
+
+
+room.init();
+module.exports=room;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const exphbs  = require('express-handlebars');
+const model = require("./models/room");
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 app.use(express.static('public'));
@@ -13,7 +14,8 @@ app.get('/', function (req, res) {
     res.render("homepage",{
         title:"Home",
         headingInfo:"HOMEPAGE",
-        randomContent:"bkjsbjbsajf"
+        randomContent:"bkjsbjbsajf",
+        room : model.getallFeaturedRooms()
     })
   })
 
@@ -118,63 +120,11 @@ app.get('/', function (req, res) {
     })
   })
 
-  app.get('/explore', function (req, res) {
-    const places=[];
-    places.push(
-      {
-        imgURL:`/img/room1.jpg`,
-        title:`ENTIRE VILLA. 2 BEDS | Modern Branyan Guest Villa`,
-        price:100,
-        rating:5
-      });
-      places.push(
-        {
-          imgURL:`/img/room2.jpg`,
-          title:`ENTIRE HOUSE · 8 BEDS | Spacious Coastal Retreat`,
-          price:120,
-          rating:4.1
-  
-        });
-        places.push(
-          {
-            imgURL:`/img/room3.jpg`,
-            title:`ENTIRE BUNGALOW · 3 BEDS | Point Boston Beach House`,
-            price:200,
-            rating:5
-    
-          });
-        
-          places.push(
-            {
-              imgURL:`/img/room4.jpg`,
-              title:`ENTIRE BUNGALOW · 4 BEDS | Beach Front Holiday Home `,
-              price:140,
-              rating:4.5
-      
-            });
-        
-            places.push(
-              {
-                imgURL:`/img/room5.jpg`,
-                title:`ENTIRE APARTMENT · 2 BEDS | The Loft - Crackenback`,
-                price:1270,
-                rating:4.4
-        
-              });
-        
-              places.push(
-                {
-                  imgURL:`/img/room6.jpg`,
-                  title:`ENTIRE APARTMENT · 4 BEDS |The Beach Apartment `,
-                  price:190,
-                  rating:4.3
-          
-                });
-                
+  app.get('/explore', function (req, res) {              
     res.render("explore",{
       title:"List of properties",
         headingInfo:"Properties ready to rent",
-        rooms:places
+        room : model.getallListingRoom()
     })
   })
 
